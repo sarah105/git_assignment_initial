adbc914681f02c71311eabf1771cf771e94536c3
RahmaRamadan <36454500+RahmaRamadan@users.noreply.github.com>
2020-02-28 17:40:39 +0200

    enhancing to reduce complexity.
    now, complexity is O(1).

--- GitAssignment/src/SummationSeriesSubscriber.java
+++ GitAssignment/src/SummationSeriesSubscriber.java
@@ -5,15 +5,12 @@ public class SummationSeriesSubscriber implements ISubscriber {
 	public void notifySubscriber(String input) {
 		System.out.println("Hello, I am summation series subscriber and I am notified with " + input);		
 		try {
-			int sum = 0, num = Integer.parseInt(input);		
-			 for (int i = 0; i <= num; i++) 
-			    {
-			            sum += i;
-			    }
-			    System.out.println("The Summation of series is = "+sum);
-		}	
-		catch (Exception e) {
-			System.out.println("Invalid '"+ input +"' is not a number.");
+		        int num = Integer.parseInt(input), sum= num*(num+1)/2;
+			System.out.println("The Summation of series is = "+sum);
+		    }	
+		catch (Exception e)
+		{
+		        System.out.println("Invalid '"+ input +"' is not a number.");
 		}		
 	}
 }
